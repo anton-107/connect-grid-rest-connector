9eae7d09490478862c9853e0e513676e19fda913
Anton Nekipelov <anton.nekipelov@gmail.com>
2014-11-22 14:20:45 +0100

    basic structure (pass options to directive)

---
+++ js/grid-rest-connector-directive.js
@@ -0,0 +1,32 @@
+(function (angular, _) {
+    'use strict';
+
+    angular.module('connect-grid-rest-connector').directive('connectGridRestConnector', [function () {
+
+
+
+        return {
+            restrict: 'A',
+            compile: function () {
+                return {
+                    pre: function (scope, element, attrs) {
+                        var options = scope.$eval(attrs.connectGridRestConnector);
+                        var Resource = options.Resource;
+
+                        if (!Resource) {
+                            return;
+                        }
+
+                        Resource.query();
+
+                        console.log('options', options);
+                    },
+                    post: function () {
+                        console.log('post');
+                    }
+                }
+            }
+        };
+
+    }]);
+})(window.angular, window._);
